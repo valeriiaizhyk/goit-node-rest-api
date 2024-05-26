6814df5663a6f20dceb34d0517f18359ad4a4b3e
valeriiaizhyk <137833881+valeriiaizhyk@users.noreply.github.com>
2024-05-26 23:25:56 +0300

    contacts.js/user.js

--- models/contacts.js
+++ models/contacts.js
@@ -16,6 +16,10 @@ const contactSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
   },
   {
     versionKey: false,
